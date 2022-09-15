8a6be72c164a68d623b3787de16f81af1db43ee9
Andrew Maule <developer@bliptrip.net>
2022-09-15 09:47:29 -0500

    Changes to SCC encoder such that it truly implements the chain code encoding as defined in respective paper.

--- ztools/dip/chaincodes/scc.py
+++ ztools/dip/chaincodes/scc.py
@@ -3,20 +3,12 @@
 # Objective: Calculates the slope chain code on a berry contour, as defined
 # in Bribiesca et al.'s 2013 work: https://doi.org/10.1016/j.patcog.2012.09.017
 #
-# NOTE: This is a slightly different version of SCC that is easier to code
-#           in python using the shapely library.
-#       Rather than overlaying a circle along the path of the contours, I use
-#           the shapely library function 'simplify' to reduce discretize a
-#           polygon.  From there, I traverse the points of the simplified
-#           contour and calculate slope changes as I traverse.
-#   The goal here is to calculate the shape's tortuosity.
-#
 
 import argparse
 import logging
 import math
 import numpy as np
-from shapely.geometry import LinearRing
+from shapely.geometry import LineString, LinearRing, Point, MultiPoint
 import sys
 
 # construct the argument parser and parse the arguments
@@ -25,42 +17,100 @@ def parse_args():
     parser.add_argument('-i', '--input', action='store', type=str, required=True, help="Path to input file containing numpy-formatted array of contours for berry shape.")
     parser.add_argument('-o', '--output', action='store', type=str, required=True, help="Path to output file to store numpy-formatted array of SCC.")
     parser.add_argument('-l', '--level', type=str, default="WARNING", choices=["DEBUG","INFO","WARNING","ERROR","CRITICAL"], help="Set the logging level for the SCC encoder (debugging purposes).")
-    parser.add_argument('-t', '--tolerance', type=float, default=0.5, help="Starting tolerance for simplifying berry contours before calculating SCC - this tool will progressively calculate tortuosity at lower and lower "
-                            "tolerances until it detects no change -- it will chose the tolerance with the highest tolerance/simplist representation of the berry contour.")
+    parser.add_argument('-n', '--num-segments', dest='segments', default=None, type=int, help="Number of SCC segments to divide contours up into.")
+    parser.add_argument('--segment-length', dest='seglength', default=None, type=float, help="Segment lengths")
     parsed = parser.parse_args(sys.argv[1:])
     return(parsed)
 
+
+def generateTransectCircle(radius, center=(0,0), num_points=64):
+    points = []
+    for theta in np.linspace(start=0, stop=2*math.pi, num=num_points, endpoint=False, dtype='float'):
+        points.append((center[0] + radius * math.cos(theta), center[1] + radius * math.sin(theta)))
+    circle = LinearRing(points)
+    return(circle)
+
+
 class SCCEncoder(object):
-    def __init__(self, logger=None, tolerance=0.1, contours=None, SCC=None):
+    def __init__(self, logger=None, segment_length=None, num_segments=None, contours=None, SCC=None, precision=10e-1):
+        self.points = [] #Points along original contour that are used for generating SCC
+        self.circles = []
+        self.precision = precision
         if SCC is not None:
             self.SCC  = SCC
         else:
             self.SCC = []
         self.logger = logger
         if contours is not None:
-            self.encode(contours, tolerance)
+            self.encode(contours, segment_length, num_segments)
 
-    def encode(self, contours, tolerance):
-        #Shift contours to have the max y contour be the first point to begin encoding traversal process
-        contours = LinearRing(contours)
-        if( contours.is_ccw ):
-            contours.coords = list(contours.coords)[::-1] #Make clockwise
-        contours_simple = contours.simplify(tolerance)
-        #Traverse, calculating slopes
-        contours_simple_a = np.asarray(contours_simple)
-        if(np.all(contours_simple_a[0] == contours_simple_a[-1])): #If we have a repeat at start and end, remove it
-            contours_simple_a = contours_simple_a[0:-1]
+    def encode(self, contours, segment_length=None, num_segments=None):
+        #Reset
         self.SCC = []
-        for i in range(0, len(contours_simple_a)): #Start from 1 as we assume start/end are equivalent
-            past = contours_simple_a[i-1]
-            current = contours_simple_a[i]
-            future = contours_simple_a[(i+1)%len(contours_simple_a)]
-            diff1 = current - past
-            slope_1 = math.atan2(diff1[0],diff1[1])
-            diff2 = future - current
-            slope_2 = math.atan2(diff2[0],diff2[1])
-            alpha = (slope_2 - slope_1)/math.pi #Normalize to [-1,1]
-            self.SCC.append(alpha)
+        self.points = []
+        self.circles = []
+        #Shift contours to have the max y contour be the first point to begin encoding traversal process
+        circles = []
+        scc_points = []
+        contours = LineString(contours)
+        if( (segment_length != None) and (num_segments != None) ):
+            assert(False, "Both segment length and number of segments are defined.  Only once should be defined.")
+        if segment_length != None:
+            radius = segment_length
+        elif num_segments != None:
+            radius = contours.length/num_segments
+        else:
+            assert(False, "Neither segment length or number of segments are defined.  One and only one needs to be defined.")
+        if contours.length <= radius:
+            return
+        if(np.all(contours.coords[0] == contours.coords[-1])): #If we have a repeat at start and end, remove it
+            contours = LineString(contours.coords[0:-1])
+        circle_base = generateTransectCircle(radius)
+        n = Point(np.round(contours.coords[0],decimals=2)) #Grab the first contour point as the first point of scc
+        distances_total = [0.0]
+        while (distances_total[-1] <= (1.10*contours.length)) and (n is not None): #Add 10% to contours length as the SCC line segments and contours
+            scc_points.append(n)
+            circle = LinearRing(np.array(circle_base.coords) + np.array(n.coords))
+            circles.append(circle)
+            ints = contours.intersection(circle)
+            if type(ints) == Point: 
+                nps = [ints]
+            elif type(ints) == MultiPoint:
+                nps = ints.geoms
+            else:
+                break
+            distances = []
+            nps = [(round(p.x,2),round(p.y,2)) for p in nps]
+            for e in nps:
+                distances.append(contours.project(Point(e)))
+            distances = np.round(np.array(distances), decimals=2)
+            d_tf = distances > np.max(distances_total)
+            nps = np.array(nps)[d_tf]
+            distances = distances[d_tf]
+            distances_sorted_i = np.argsort(distances)
+            n = None
+            for i in distances_sorted_i:
+                cp = Point(nps[i,:])
+                if cp not in scc_points:
+                    distances_total.append(distances[i])
+                    n = cp
+                    break
+        if len(scc_points) > 2:
+            scc_line = LineString(scc_points)
+            diffs = np.array(scc_line.coords[0:] + [scc_line.coords[0]]) - np.array([scc_line.coords[-1]] + scc_line.coords[0:])
+            slopes = np.arctan2(diffs[:,1], diffs[:,0])
+            slopes_alpha = slopes / math.pi
+            alphas = slopes_alpha[1:] - slopes_alpha[0:-1]
+            slopes_adjust = np.any((alphas > 1) | (alphas < -1))
+            while slopes_adjust == True:
+                alphas[alphas > 1] = alphas[alphas > 1] - 2 #2 represents one full rotation/cycle on polar coordinates
+                alphas[alphas < -1] = alphas[alphas < -1] + 2 #2 represents one full rotation/cycle on polar coordinates
+                slopes_adjust = np.any((alphas > 1) | (alphas < -1)) #Any slopes still need adjusting so that they are within [-1,1] range?
+            self.SCC = [a for a in alphas]
+            self.points = scc_points
+            self.diffs = diffs
+            self.slopes = slopes
+            self.circles = circles
         return
 
     def tortuosity(self, normalize=False):
@@ -73,8 +123,10 @@ class SCCEncoder(object):
         return(np.array(self.SCC))
 
     def __str__(self):
-        return(','.join(self.SCC))
-
+        #Discretize the output into a string of characters 
+        charsb = (((np.array(self.SCC, dtype='float') - (-1))/2)/self.precision) + 0x400 #0x400 is the beginning of the cyrillic alphabet, which offers 256 contiguous character options
+        chars = bytes(charsb.astype('uint32')).decode('utf32')
+        return(chars)
 
 if __name__ == '__main__':
     logger      = logging.getLogger()
@@ -83,17 +135,17 @@ if __name__ == '__main__':
     logger.setLevel(decoded_level)
     contours    = np.load(parsed.input)
     highest_tolerance = parsed.tolerance
-    tolerance = parsed.tolerance
-    scc        = SCCEncoder(logger=logger,contours=contours,tolerance=tolerance)
+    num_segments = parsed.segments
+    scc        = SCCEncoder(logger=logger,contours=contours,num_segments=num_segments)
     highest_tortuosity = scc.tortuosity()
     while( True ):
-        tolerance = tolerance/2
-        scc.encode(contours=contours, tolerance=tolerance)
+        num_segments = num_segments/2
+        scc.encode(contours=contours, num_segments=num_segments)
         new_tortuosity = scc.tortuosity()
         if( scc.tortuosity() > 1.025 * highest_tortuosity ): #Give ourselves some buffer room on this
-            highest_tolerance = tolerance
+            highest_num_segments = num_segments
             highest_tortuosity = new_tortuosity
         else:
             break
-    scc.encode(contours=contours, tolerance=highest_tolerance)
-    print("File: {},: Tolerance: {}, Tortuosity: {} ({})".format(parsed.input,highest_tolerance,scc.tortuosity(),scc.tortuosity(True)))
+    scc.encode(contours=contours, num_segments=highest_num_segments)
+    print("File: {},: Number of SCC Segments: {}, Tortuosity: {} ({})".format(parsed.input,highest_num_segments,scc.tortuosity(),scc.tortuosity(True)))
